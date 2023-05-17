04e92f115aeba15d6ede7119490d23292f87bdf1
Stephen Pino~ <you@example.com>
2023-05-17 18:49:23 +0100

    agecalculator script added. JS validation.

--- script.js
+++ script.js
@@ -57,57 +57,68 @@ function ageCalculator() {
         let userMonth = document.getElementById("month").value;
         let userDay = document.getElementById("day").value; 
         
-        let dob = new Date(userYear, userMonth - 1, userDay);
+        //let userinput = new Date(userYear, userMonth - 1, userDay);
         
-        result = document.getElementById("result");
+        let result = document.getElementById("yearmessage");
     
         //Check date value is a proper date.
         if(FutureDate(userYear.value))
         {
             result.innerHTML = "Must be in the past";
-            return true;
+            return false;
         } 
 
         //check user provide input or not  
-        if(userinput==null || userinput==''){  
-          document.getElementById("message").innerHTML = "**Choose a date please!";    
+        if(userYear==null || userYear==''){  
+          document.getElementById("yearmessage").innerHTML = "**Choose a year please!";    
           return false;   
-        }   
-          
+        }
+        else if(userMonth==null || userMonth==''){
+
+            document.getElementById("monthmessage").innerHTML = "**Choose a month please!";    
+          return false;
+        }
+        else if(userDay==null || userDay==''){
+
+            document.getElementById("daymessage").innerHTML = "**Choose a day please!";    
+          return false;
+        }
         //execute if user entered a date   
         else {  
         //extract and collect only date from date-time string  
-        var mdate = userinput.toString();  
-        var dobYear = parseInt(mdate.substring(0,4), 10);  
-        var dobMonth = parseInt(mdate.substring(5,7), 10);  
-        var dobDate = parseInt(mdate.substring(8,10), 10);  
+       // let mdate = userinput.toString();  
+        //let dobYear = parseInt(mdate.substring(0,4), 10);  
+        //let dobMonth = parseInt(mdate.substring(5,7), 10);  
+       // let dobDate = parseInt(mdate.substring(8,10), 10);  
           
         //get the current date from system  
-        var today = new Date();  
+        let today = new Date();  
         //date string after broking  
-        var birthday = new Date(dobYear, dobMonth-1, dobDate);  
+        //let birthday = new Date(dobYear, dobMonth-1, dobDate);
+        let birthday = new Date(userYear, userMonth - 1, userDay);
           
         //calculate the difference of dates  
-        var diffInMillisecond = today.valueOf() - birthday.valueOf();  
+        let diffInMillisecond = today.valueOf() - birthday.valueOf();  
       
         //convert the difference in milliseconds and store in day and year variable          
-        var year_age = Math.floor(diffInMillisecond / 31536000000);  
-        var day_age = Math.floor((diffInMillisecond % 31536000000) / 86400000);  
+        let year_age = Math.floor(diffInMillisecond / 31536000000);  
+        let day_age = Math.floor((diffInMillisecond % 31536000000) / 86400000);  
       
         //when birth date and month is same as today's date        
         if ((today.getMonth() == birthday.getMonth()) && (today.getDate() == birthday.getDate())) {  
-                alert("Happy Birthday!");  
+                //alert("Happy Birthday!");
+            document.getElementById("result").innerHTML = "happy Birthday!";  
             }  
               
-         var month_age = Math.floor(day_age/30);          
+         let month_age = Math.floor(day_age/30);          
          day_ageday_age = day_age % 30;  
               
-         var tMnt= (month_age + (year_age*12));  
-         var tDays =(tMnt*30) + day_age;  
+         let tMnt= (month_age + (year_age*12));  
+         let tDays =(tMnt*30) + day_age;  
            
         //DOB is greater than today's date, generate an error: Invalid date    
-         if (dob>today) {  
-            document.getElementById("result").innerHTML = ("Invalid date input - Please try again!");  
+         if (birthday>today) {  
+            document.getElementById("result").innerHTML = "Invalid date input - Please try again!";  
           }  
           else {  
             document.getElementById("result").innerHTML = year_age + " years " + month_age + " months " + day_age + " days"  

69ffbcffc2e53188c3e3ff8710e372faf78c5a6a
Stephen Pino~ <you@example.com>
2023-05-17 08:08:30 +0100

    list created

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
 
   function validateForFutureDate(){
     
-    var userYear = document.getElementById("year");
+    let userYear = document.getElementById("year").value;
+    let userMonth = document.getElementById("month").value;
+    let userDay = document.getElementById("day").value;    
 
     result = document.getElementById("result");
-    regExp = /(0[1-9]|[12][0-9]|3[01])[\/](0[1-9]|1[012])[\/]201[4-9]|20[2-9][0-9]/;
     
-        //Check it is a valid date...
-        if(!regExp.test(userYear.value)){
 
-        result.innerHTML = "Invalid date!";
-        result.style.color = "red";
-
-    return;
-
+    if(FutureDate(userYear.value))
+    {
+        result.innerHTML = "Must be in the past";
+        return true;
+    } 
+    else {
+        //result.innerHTML = " Entered date is not a future date";
+        //let currentYear = new Date().getFullYear();
+        //let monthnow = new Date().getTime().getMonth();
+        //let yearAge = currentYear - userYear;
+        //let month = monthnow - userMonth
+        
+        let birthdate = new Date(userYear, userMonth - 1, userDay);
+        //calculate month difference from current date in time  
+        let month_diff = Date.now() - birthdate.getTime();  
+        
+        //convert the calculated difference in date format  
+        let age_dt = new Date(month_diff);   
+        
+        //extract year from date      
+        let year = age_dt.getUTCFullYear();  
+        
+        //now calculate the age of the user  
+        let age = Math.abs(year - 1970);  
+        
+        //display the calculated age  
+        //document.write("Age of the date entered: " + age + " years");  
+        let outputage = document.getElementById("yearOutput");
+        let outputMonth = document.getElementById("monthOutput");
+        let outputYear = document.getElementById("dayOutput");
+        
+        
+        
+        
+        //return false;
     }
-
-        if(FutureDate(userYear.value))
-        {
-            result.innerHTML = "Must be in the past";
-            return true;
-        } 
-        else {
-            result.innerHTML = " Entered date is not a future date";
-            return false;
-        }
-    }
+}
 
 
     function FutureDate(userYear){

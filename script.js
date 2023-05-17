823ddbdd239e84341f5a6418b73263873cd7de64
Stephen Pino~ <you@example.com>
2023-05-17 19:37:18 +0100

    validation added.

--- script.js
+++ script.js
@@ -1,52 +1,3 @@
-
-  function validateForFutureDate(){
-    
-    let userYear = document.getElementById("year").value;
-    let userMonth = document.getElementById("month").value;
-    let userDay = document.getElementById("day").value;    
-
-    result = document.getElementById("result");
-    
-
-    if(FutureDate(userYear.value))
-    {
-        result.innerHTML = "Must be in the past";
-        return true;
-    } 
-    else {
-        //result.innerHTML = " Entered date is not a future date";
-        //let currentYear = new Date().getFullYear();
-        //let monthnow = new Date().getTime().getMonth();
-        //let yearAge = currentYear - userYear;
-        //let month = monthnow - userMonth
-        
-        let birthdate = new Date(userYear, userMonth - 1, userDay);
-        //calculate month difference from current date in time  
-        let month_diff = Date.now() - birthdate.getTime();  
-        
-        //convert the calculated difference in date format  
-        let age_dt = new Date(month_diff);   
-        
-        //extract year from date      
-        let year = age_dt.getUTCFullYear();  
-        
-        //now calculate the age of the user  
-        let age = Math.abs(year - 1970);  
-        
-        //display the calculated age  
-        //document.write("Age of the date entered: " + age + " years");  
-        let outputage = document.getElementById("yearOutput");
-        let outputMonth = document.getElementById("monthOutput");
-        let outputYear = document.getElementById("dayOutput");
-        
-        
-        
-        
-        //return false;
-    }
-}
-
-
 function ageCalculator() { 
     
         //collect input from HTML form and convert into date format  
@@ -70,26 +21,26 @@ function ageCalculator() {
 
         //check user provide input or not  
         if(userYear==null || userYear==''){  
-          document.getElementById("yearmessage").innerHTML = "**Choose a year please!";    
+          document.getElementById("yearmessage").innerHTML = "**Must be a valid year";    
           return false;   
         }
         else if(userMonth==null || userMonth==''){
 
-            document.getElementById("monthmessage").innerHTML = "**Choose a month please!";    
+            document.getElementById("monthmessage").innerHTML = "**Must be a valid month";    
           return false;
         }
         else if(userDay==null || userDay==''){
 
-            document.getElementById("daymessage").innerHTML = "**Choose a day please!";    
+            document.getElementById("daymessage").innerHTML = "**Must be a valid day";    
           return false;
         }
         //execute if user entered a date   
         else {  
         //extract and collect only date from date-time string  
-       // let mdate = userinput.toString();  
+        //let mdate = userinput.toString();  
         //let dobYear = parseInt(mdate.substring(0,4), 10);  
         //let dobMonth = parseInt(mdate.substring(5,7), 10);  
-       // let dobDate = parseInt(mdate.substring(8,10), 10);  
+        //let dobDate = parseInt(mdate.substring(8,10), 10);  
           
         //get the current date from system  
         let today = new Date();  
@@ -107,7 +58,7 @@ function ageCalculator() {
         //when birth date and month is same as today's date        
         if ((today.getMonth() == birthday.getMonth()) && (today.getDate() == birthday.getDate())) {  
                 //alert("Happy Birthday!");
-            document.getElementById("result").innerHTML = "happy Birthday!";  
+            document.getElementById("happybdaymessage").innerHTML = "happy Birthday!";  
             }  
               
          let month_age = Math.floor(day_age/30);          
@@ -118,10 +69,16 @@ function ageCalculator() {
            
         //DOB is greater than today's date, generate an error: Invalid date    
          if (birthday>today) {  
-            document.getElementById("result").innerHTML = "Invalid date input - Please try again!";  
+            document.getElementById("yearmessage").innerHTML = "Invalid date input - Please try again!";  
           }  
           else {  
-            document.getElementById("result").innerHTML = year_age + " years " + month_age + " months " + day_age + " days"  
+            //document.getElementById("result").innerHTML = year_age + " years " + month_age + " months " + day_age + " days"
+            document.getElementById("yearresult").innerHTML = "";
+            document.getElementById("monthresult").innerHTML = "";
+            document.getElementById("dayresult").innerHTML = "";
+            document.getElementById("yearresult").innerHTML = year_age + " years ";  
+            document.getElementById("monthresult").innerHTML = month_age + " months ";  
+            document.getElementById("dayresult").innerHTML = day_age + " days";    
           }  
     }  
 }
